1eb0423bf466fd4b91b9ca622ccc4a0c6c9cfb70
Alyxia Sother <alyxia@riseup.net>
2022-07-07 13:38:34 +0000

    [Sandbox] Add plugin, has Flux logger

---
+++ Sandbox/src/index.tsx
@@ -0,0 +1,33 @@
+import { Plugin, registerPlugin } from 'enmity/managers/plugins';
+import { getByProps } from 'enmity/metro';
+import { makeStore } from 'enmity/api/settings';
+import { create } from 'enmity/patcher';
+import manifest from '../manifest.json';
+
+import Settings from './components/Settings';
+
+const FluxDispatcher = getByProps('_currentDispatchActionType', '_subscriptions', '_waitQueue');
+const Patcher = create('sandbox');
+
+const SilentTyping: Plugin = {
+   ...manifest,
+
+   onStart() {
+    const Settings = makeStore(this.name);
+    Patcher.after(FluxDispatcher, 'dispatch', (_, ...args) => {
+      if (Settings.get('sandboxLogAll', false)) {
+        console.log(args[0]);
+      }
+    });
+   },
+
+   onStop() {
+      Patcher.unpatchAll();
+   },
+
+   getSettingsPanel({ settings }) {
+      return <Settings settings={settings} />;
+   }
+};
+
+registerPlugin(SilentTyping);
